fe7b6c961848b6ff222dbb98661b54179508334c
Niteshjain54 <96040095+Niteshjain54@users.noreply.github.com>
2022-01-14 18:34:15 +0530

    Update decimaltobinary.java

--- decimaltobinary.java
+++ decimaltobinary.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class decimaltobinary {
     public static void main (String[] args){
         Scanner scn= new Scanner (System.in);
         int n=scn.nextInt();
