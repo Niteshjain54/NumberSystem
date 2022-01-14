0f38731dc5d4578a68aaeb84e9c10f466ad6b1da
niteshjain54 <niteshjain54@gmail.com>
2022-01-14 18:31:42 +0530

    n

---
+++ decimaltobinary.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args){
+        Scanner scn= new Scanner (System.in);
+        int n=scn.nextInt();
+        
+        int ans=0,pow=1;
+        while(n!=0){
+            int rem=n%2;
+            n /=2;
+            ans +=rem*pow;
+            pow= pow*10;
+        }
+        System.out.print(ans);
+    }
+}
